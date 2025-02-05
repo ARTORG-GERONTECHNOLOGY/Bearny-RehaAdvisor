ff1a365e2a63834dd5c81253087a1064d44c2a43
Noora Angelva <noora.angelva@unibe.ch>
2025-02-05 07:01:40 +0000

    Update file config.py

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import json
 # Start from the current file's directory and go up three levels
 # Calculate the path to the 'config' directory
-config_dir = Path(__file__).parent / "config" / "config.json"
+config_dir = Path(__file__).parent.parent / "config" / "config.json"
 
 with open(config_dir, 'r') as f:
     config = json.load(f)
