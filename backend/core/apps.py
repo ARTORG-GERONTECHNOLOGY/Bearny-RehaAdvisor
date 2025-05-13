663bd2c8e637eea55900bade98d6dea7b6cbf386
Bot <michael.single@unibe.ch>
2025-05-13 07:56:20 +0000

    Dev and Prod Separeated and running in https.

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -9,14 +9,11 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         connect(
-            db="admin",
+            db=os.environ.get("DB_NAME", "myappdb"),  # <<< new!
             host=os.environ.get("DB_HOST", "localhost"),
             port=int(os.environ.get("DB_PORT", 27017)),
-            username=os.environ.get(
-                "MONGO_INITDB_ROOT_USERNAME", "root"
-            ),  # only if using authentication
-            password=os.environ.get(
-                "MONGO_INITDB_ROOT_PASSWORD", "example"
-            ),  # only if using authentication
+            username=os.environ.get("MONGO_INITDB_ROOT_USERNAME", "root"),
+            password=os.environ.get("MONGO_INITDB_ROOT_PASSWORD", "example"),
             authentication_source="admin",
         )
+
