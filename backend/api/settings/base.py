a56d91393f5e6c48f9654c2a20cf62197bce0feb
Noora Angelva <noora.angelva@unibe.ch>
2025-05-14 15:23:55 +0000

    Resolve "Email Authetication and notifications"

--- backend/api/settings/base.py
+++ backend/api/settings/base.py
@@ -76,3 +76,18 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "smtp.office365.com")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", 587))
+
+# Use TLS on port 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "info@reha-advisor.ch")
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"] #"nrk37CAYsTW&3sJ"
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
