bff2c73ef3df35b3f8008fef55b7a1fa47fb3e0c
Noora Angelva <noora.angelva@unibe.ch>
2025-07-04 10:57:23 +0000

    Resolve "Celestia Jobs"

--- backend/utils/config.py
+++ backend/utils/config.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 # Start from the current file's directory and go up three levels
 # Calculate the path to the 'config' directory
-config_dir = Path(__file__).parent.parent / "config" / "config.json"
+config_dir = Path(__file__).parent.parent / "config.json"
 
 with open(config_dir, "r") as f:
     config = json.load(f)
