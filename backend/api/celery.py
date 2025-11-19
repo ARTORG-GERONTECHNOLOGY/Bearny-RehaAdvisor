e5ca7ab2af2582a26fd7427358383494a6bbe9f8
nooraangelva <nooraangelva@gmail.com>
2025-11-19 12:42:52 +0000

    feat: Enhance User Profile Editing and Header Navigation
    - Improved EditTherapistInfo component with password change functionality and validation.
    - Added StatusBanner component for displaying success and error messages in UserProfile.
    - Updated Header component for better navigation and mobile responsiveness.
    - Introduced disease filtering in Therapist page.
    - Fixed minor typos in config.json and improved code formatting across components.

--- backend/api/celery.py
+++ backend/api/celery.py
@@ -1,7 +1,17 @@
 # api/celery.py
 import os
 from celery import Celery
+from mongoengine import connect
+import os
 
+connect(
+    db=os.environ.get("DB_NAME", "myappdb"),
+    host=os.environ.get("DB_HOST", "localhost"),
+    port=int(os.environ.get("DB_PORT", 27017)),
+    username=os.environ.get("MONGO_INITDB_ROOT_USERNAME", "root"),
+    password=os.environ.get("MONGO_INITDB_ROOT_PASSWORD", "example"),
+    authentication_source="admin",
+)
 # Must come before Celery starts
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "api.settings.base")
 
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import i18n from '../i18n'; // Import the i18n config
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 import { I18nextProvider } from 'react-i18next';
 import { Router } from './routes';
