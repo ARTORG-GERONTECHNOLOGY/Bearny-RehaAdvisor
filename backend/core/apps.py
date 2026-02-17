61006a24a8e67293e3213634663b7de042ba98e6
nooraangelva <nooraangelva@gmail.com>
2026-02-17 08:37:03 +0000

    feat: enhance MongoDB configuration with TLS support and user creation script

--- backend/core/apps.py
+++ backend/core/apps.py
@@ -11,9 +11,11 @@ class CoreConfig(AppConfig):
         connect(
             db=os.environ.get("DB_NAME"),
             host=os.environ.get("DB_HOST"),
-            port=int(os.environ.get("DB_PORT")),
-            username=os.environ.get("MONGO_INITDB_ROOT_USERNAME",),
+            port=int(os.environ.get("DB_PORT", "27017")),
+            username=os.environ.get("MONGO_INITDB_ROOT_USERNAME"),
             password=os.environ.get("MONGO_INITDB_ROOT_PASSWORD"),
-            authentication_source=os.environ.get("MONGO_INITDB_AUTH_SOURCE"),
+            authentication_source=os.environ.get("MONGO_INITDB_AUTH_SOURCE", "admin"),
+            tls=True,
+            tlsCAFile="/etc/ssl/mongo/ca.crt",
         )
 
