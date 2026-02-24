866200392c42d101360fb7698aa40ad686f46b40
naefdominik <naef.dominik@hotmail.com>
2026-02-24 21:16:58 +0100

    feat(Navigation): use svg icons instead of lucide-react

--- frontend/src/components/NavItem.tsx
+++ frontend/src/components/NavItem.tsx
@@ -1,13 +1,24 @@
 type Props = {
   onClick?: () => void;
-  icon: React.ComponentType<{ size: number }>;
+  iconOutline: React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  iconFill: React.ComponentType<React.SVGProps<SVGSVGElement>>;
   label: string;
   active?: boolean;
   desktop?: boolean;
 };
 
 // TODO: move colors to config as soon as they are fixed
-export function NavItem({ onClick, icon: Icon, label, active = false, desktop = false }: Props) {
+export function NavItem({
+  onClick,
+  iconOutline: IconOutline,
+  iconFill: IconFill,
+  label,
+  active = false,
+  desktop = false,
+}: Props) {
+  const Icon = active ? IconFill : IconOutline;
+  const iconSize = desktop ? 'w-5 h-5' : 'w-6 h-6';
+
   return (
     <button
       onClick={onClick}
@@ -20,7 +31,7 @@ export function NavItem({ onClick, icon: Icon, label, active = false, desktop = 
         ${active && desktop ? 'bg-[#E6E6E6] rounded-full' : 'bg-transparent'}
       `}
     >
-      <Icon size={desktop ? 20 : 24} />
+      <Icon className={iconSize} />
       <span>{label}</span>
     </button>
   );
