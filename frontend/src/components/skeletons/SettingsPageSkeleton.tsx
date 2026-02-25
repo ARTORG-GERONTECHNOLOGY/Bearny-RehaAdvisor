d0c67accafd0ce2203dcd5b9e2ecc286ee91637b
naefdominik <naef.dominik@hotmail.com>
2026-02-25 22:25:53 +0100

    feat(Settings): prepare notification card

--- frontend/src/components/skeletons/SettingsPageSkeleton.tsx
+++ frontend/src/components/skeletons/SettingsPageSkeleton.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import Layout from '@/components/Layout';
 import { Skeleton } from '@/components/ui/skeleton';
 
 const SettingsPageSkeleton: React.FC = () => (
-  <div className="min-h-screen">
-    <main className="flex-1">
-      <div className="container mx-auto px-4 pb-6 pt-36 max-w-4xl">
-        <Skeleton className="h-16 w-full" />
+  <Layout>
+    <div className="container mx-auto max-w-[90%] md:max-w-screen-md">
+      {/* Title skeleton */}
+      <div className="pt-16 md:pt-0 flex items-center gap-[6px]">
+        <Skeleton className="w-6 h-6 rounded" />
+        <Skeleton className="h-7 w-32" />
       </div>
-    </main>
-  </div>
+
+      {/* Settings card skeleton */}
+      <div className="mt-4 bg-[#F9F9F9] border border-[#D4D4D4] rounded-[16px] p-5 flex flex-col gap-[2px]">
+        <Skeleton className="h-5 w-24" />
+        <Skeleton className="h-6 w-16" />
+      </div>
+    </div>
+  </Layout>
 );
 
 export default SettingsPageSkeleton;
