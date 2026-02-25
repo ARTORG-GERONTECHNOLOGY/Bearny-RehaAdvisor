ce43f3567f78b7cd4675e54f13e00c3ae8df23d1
naefdominik <naef.dominik@hotmail.com>
2026-02-25 22:10:36 +0100

    feat(Settings): create initial page for notification management

--- frontend/src/components/skeletons/SettingsPageSkeleton.tsx
+++ frontend/src/components/skeletons/SettingsPageSkeleton.tsx
@@ -1,4 +1,19 @@
 import React from 'react';
+import { Skeleton } from '@/components/ui/skeleton';
+
+const SettingsPageSkeleton: React.FC = () => (
+  <div className="min-h-screen">
+    <main className="flex-1">
+      <div className="container mx-auto px-4 pb-6 pt-36 max-w-4xl">
+        <Skeleton className="h-16 w-full" />
+      </div>
+    </main>
+  </div>
+);
+
+export default SettingsPageSkeleton;
+
+import React from 'react';
 import Layout from '@/components/Layout';
 import { Skeleton } from '@/components/ui/skeleton';
 
