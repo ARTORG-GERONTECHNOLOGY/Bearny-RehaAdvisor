077e50c67cf4d91d676cb1bd32d871bcbc2d0c16
naefdominik <naef.dominik@hotmail.com>
2026-03-01 15:28:02 +0100

    feat(App): add scope to sw registration

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -22,7 +22,7 @@ root.render(
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/sw.js')
+      .register('/sw.js', { scope: '/' })
       .then((registration) => console.log('SW registered:', registration))
       .catch((registrationError) => console.log('SW registration failed:', registrationError));
   });
