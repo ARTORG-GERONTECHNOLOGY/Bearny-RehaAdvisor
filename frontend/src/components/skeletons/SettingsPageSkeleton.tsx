6e0566b7134da99929965a138fb0948314347cc4
naefdominik <naef.dominik@hotmail.com>
2026-03-01 12:56:48 +0100

    feat(Layout): add container component

--- frontend/src/components/skeletons/SettingsPageSkeleton.tsx
+++ frontend/src/components/skeletons/SettingsPageSkeleton.tsx
@@ -2,35 +2,6 @@ import React from 'react';
 import Layout from '@/components/Layout';
 import { Skeleton } from '@/components/ui/skeleton';
 
-const SettingsPageSkeleton: React.FC = () => (
-  <Layout>
-    <div className="container mx-auto max-w-[90%] md:max-w-screen-md">
-      {/* Title skeleton */}
-      <div className="pt-16 md:pt-0 flex items-center gap-[6px]">
-        <Skeleton className="w-6 h-6 rounded" />
-        <Skeleton className="h-7 w-32" />
-      </div>
-
-      {/* Settings card skeleton */}
-      <div className="mt-4 bg-[#F9F9F9] border border-[#D4D4D4] rounded-[16px] p-5">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col gap-1">
-            <Skeleton className="h-6 w-32" />
-            <Skeleton className="h-4 w-64" />
-          </div>
-          <Skeleton className="h-6 w-11 rounded-full" />
-        </div>
-      </div>
-    </div>
-  </Layout>
-);
-
-export default SettingsPageSkeleton;
-
-import React from 'react';
-import Layout from '@/components/Layout';
-import { Skeleton } from '@/components/ui/skeleton';
-
 export default function SettingsPageSkeleton() {
   return (
     <Layout>
