73820c4e3bd88d04cc174d0fe2b1047b99063672
naefdominik <naef.dominik@hotmail.com>
2026-03-01 16:58:54 +0100

    test(Main): use aliases

--- frontend/src/__tests__/main.test.tsx
+++ frontend/src/__tests__/main.test.tsx
@@ -1,18 +1,17 @@
-import '../../src/main'; // ✅ Import your main file directly
-jest.mock('../../src/api/client', () => require('../../src/__mocks__/api/client'));
+import '@/main';
+jest.mock('@/api/client', () => require('@/__mocks__/api/client'));
 
-// src/__tests__/main.test.tsx
-
+// Mock react-dom/client
 jest.mock('react-dom/client', () => ({
   createRoot: jest.fn(() => ({
-    render: jest.fn(), // Mock the render method
+    render: jest.fn(),
   })),
 }));
 
 describe('Main entry point', () => {
   it('renders without crashing', () => {
     expect(() => {
-      require('../../src/main'); // Import AFTER mocking createRoot
+      require('@/main');
     }).not.toThrow();
   });
 });
