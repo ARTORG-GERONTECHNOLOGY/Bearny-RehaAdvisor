bf2f959ae96313dd36b98c4a3365e190e4ceeafe
naefdominik <naef.dominik@hotmail.com>
2026-03-01 13:17:31 +0100

    feat(Settings): add cards to skeleton

--- frontend/src/components/skeletons/SettingsPageSkeleton.tsx
+++ frontend/src/components/skeletons/SettingsPageSkeleton.tsx
@@ -11,14 +11,28 @@ export default function SettingsPageSkeleton() {
         <Skeleton className="h-7 w-32" />
       </div>
 
-      {/* Settings card skeleton */}
-      <div className="mt-4 bg-[#F9F9F9] border border-[#D4D4D4] rounded-[16px] p-5">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col gap-1">
-            <Skeleton className="h-6 w-32" />
-            <Skeleton className="h-4 w-64" />
+      <div className="mt-4 flex flex-col gap-2">
+        {/* Notifications card skeleton */}
+        <div className="bg-[#F9F9F9] border border-[#D4D4D4] rounded-[16px] p-5">
+          <div className="flex items-center justify-between">
+            <div className="flex flex-col gap-1">
+              <Skeleton className="h-6 w-32" />
+              <Skeleton className="h-4 w-64" />
+            </div>
+            <Skeleton className="h-6 w-11 rounded-full" />
           </div>
-          <Skeleton className="h-6 w-11 rounded-full" />
+        </div>
+
+        {/* Language card skeleton */}
+        <div className="bg-[#F9F9F9] border border-[#D4D4D4] rounded-[16px] p-5">
+          <Skeleton className="h-6 w-24 mb-2" />
+          <Skeleton className="h-9 w-24" />
+        </div>
+
+        {/* Help card skeleton */}
+        <div className="bg-[#F9F9F9] border border-[#D4D4D4] rounded-[16px] p-5">
+          <Skeleton className="h-6 w-16 mb-2" />
+          <Skeleton className="h-10 w-20" />
         </div>
       </div>
     </Layout>
