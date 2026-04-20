4c3e81a3fc0f89f75a36df7b65884e24651a788f
naefdominik <naef.dominik@hotmail.com>
2026-04-20 22:43:23 +0200

    feat(Card): add component

---
+++ frontend/src/components/Card.tsx
@@ -0,0 +1,11 @@
+import { cn } from '@/lib/utils';
+
+type Props = React.ComponentPropsWithoutRef<'div'>;
+
+export default function Card({ children, className, ...props }: Props) {
+  return (
+    <div className={cn('p-4 border border-accent rounded-3xl', className)} {...props}>
+      {children}
+    </div>
+  );
+}
