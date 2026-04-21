5c1a5b8af6908b06d7bfc4247dd0cb7c5a96551a
naefdominik <naef.dominik@hotmail.com>
2026-04-21 15:54:39 +0200

    fix(App): remove todos

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -5,7 +5,6 @@ type Props = {
   children: React.ReactNode;
 };
 
-// TODO: move colors to config as soon as they are fixed
 export default function Layout({ children }: Props) {
   return (
     <div className="min-h-screen bg-[#ECECEC]">
