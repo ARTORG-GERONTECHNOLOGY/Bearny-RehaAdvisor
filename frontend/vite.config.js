692a7918d5c9a8924c7e587959a4e7e088d78f13
naefdominik <naef.dominik@hotmail.com>
2026-04-25 17:06:05 +0200

    feat(App): configure sentry

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
@@ -9,6 +10,9 @@ export default defineConfig(({ mode }) => {
   const isDev = mode === 'development';
 
   return {
+    build: {
+      sourcemap: true,
+    },
     plugins: [
       react(),
       svgr(),
@@ -105,6 +109,11 @@ export default defineConfig(({ mode }) => {
             },
           ],
         },
+      }),
+      sentryVitePlugin({
+        authToken: env.SENTRY_AUTH_TOKEN,
+        org: 'artorg-ger',
+        project: 'react',
       }),
     ],
     server: {
