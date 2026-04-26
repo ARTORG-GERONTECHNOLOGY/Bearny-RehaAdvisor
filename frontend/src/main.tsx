e7545bbddc48d83222838db957bc60a5a3d8295f
naefdominik <naef.dominik@hotmail.com>
2026-04-26 16:20:31 +0200

    feat(App): simplify sentry handling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -13,13 +13,8 @@ initSentry();
 
 const container = document.getElementById('root');
 const root = createRoot(container!, {
-  // Callback called when an error is thrown and not caught by an ErrorBoundary.
-  onUncaughtError: Sentry.reactErrorHandler((error, errorInfo) => {
-    console.warn('Uncaught error', error, errorInfo.componentStack);
-  }),
-  // Callback called when React catches an error in an ErrorBoundary.
+  onUncaughtError: Sentry.reactErrorHandler(),
   onCaughtError: Sentry.reactErrorHandler(),
-  // Callback called when React automatically recovers from errors.
   onRecoverableError: Sentry.reactErrorHandler(),
 });
 
