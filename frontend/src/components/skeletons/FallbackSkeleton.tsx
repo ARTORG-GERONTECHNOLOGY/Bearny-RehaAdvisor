d86c16744d67dabec025ed68074a110d7b84eac1
naefdominik <naef.dominik@hotmail.com>
2026-05-13 22:37:49 +0200

    fix(App): dont use navigation in fallback loading screen

--- frontend/src/components/skeletons/FallbackSkeleton.tsx
+++ frontend/src/components/skeletons/FallbackSkeleton.tsx
@@ -1,17 +1,19 @@
-import Layout from '@/components/Layout';
 import { Skeleton } from '@/components/ui/skeleton';
+import Container from '../Container';
 
 export default function FallbackSkeleton() {
   return (
-    <Layout>
-      <Skeleton className="h-8 w-48" />
-      <Skeleton className="h-8 w-64 mt-2" />
+    <div className="min-h-screen bg-back">
+      <Container className="pt-16 md:pt-28 transition-all">
+        <Skeleton className="h-8 w-48" />
+        <Skeleton className="h-8 w-64 mt-2" />
 
-      <div className="mt-16 space-y-3 md:w-1/2">
-        <Skeleton className="h-6 w-full" />
-        <Skeleton className="h-6 w-5/6" />
-        <Skeleton className="h-6 w-3/4" />
-      </div>
-    </Layout>
+        <div className="mt-16 space-y-3 md:w-1/2">
+          <Skeleton className="h-6 w-full" />
+          <Skeleton className="h-6 w-5/6" />
+          <Skeleton className="h-6 w-3/4" />
+        </div>
+      </Container>
+    </div>
   );
 }
