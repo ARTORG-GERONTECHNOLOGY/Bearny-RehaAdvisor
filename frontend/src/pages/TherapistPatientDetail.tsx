8bc70d942c29957e154b7b225e4e192b79b6190b
naefdominik <naef.dominik@hotmail.com>
2026-06-30 17:01:44 +0200

    fix(Tabs): handle bootstrap conflicts

--- frontend/src/pages/TherapistPatientDetail.tsx
+++ frontend/src/pages/TherapistPatientDetail.tsx
@@ -4,18 +4,21 @@ import { Separator } from '@/components/ui/separator';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { observer } from 'mobx-react-lite';
 import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { ChartColumn, Calendar, BookUser, FileQuestion } from 'lucide-react';
+import ArrowLeftIcon from '@/assets/icons/arrow-left-fill.svg?react';
 
 const TherapistPatientDetail: React.FC = observer(() => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const { patientId = '' } = useParams<{ patientId: string }>();
 
   return (
     <Layout>
+      <ArrowLeftIcon className="h-6 w-6 cursor-pointer mb-1" onClick={() => navigate(-1)} />
       <PageHeader title={patientId} />
 
-      <Separator className="my-3" />
+      <Separator className="!my-3" />
 
       <Tabs defaultValue="outcomes">
         <TabsList>
@@ -37,16 +40,16 @@ const TherapistPatientDetail: React.FC = observer(() => {
           </TabsTrigger>
         </TabsList>
         <TabsContent value="outcomes">
-          <div className="p-4">{t('Outcomes content goes here')}</div>
+          <div>{t('Outcomes content goes here')}</div>
         </TabsContent>
         <TabsContent value="rehabilitationplan">
-          <div className="p-4">{t('Rehabilitation Plan content goes here')}</div>
+          <div>{t('Rehabilitation Plan content goes here')}</div>
         </TabsContent>
         <TabsContent value="questionnaires">
-          <div className="p-4">{t('Questionnaires content goes here')}</div>
+          <div>{t('Questionnaires content goes here')}</div>
         </TabsContent>
         <TabsContent value="information">
-          <div className="p-4">{t('Information content goes here')}</div>
+          <div>{t('Information content goes here')}</div>
         </TabsContent>
       </Tabs>
     </Layout>
